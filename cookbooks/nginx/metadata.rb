d3fc674a633b762e3abbdcbddb4b367f73ab723b
ljasti <jlk.devops@gmail.com>
2016-10-24 18:06:25 +0000

    2nd cookbook

---
+++ cookbooks/nginx/metadata.rb
@@ -0,0 +1,8 @@
+name             'nginx'
+maintainer       'INNOMINDS SOFTWARE PVT LTD'
+maintainer_email 'ljasti@innominds.com'
+license          'All rights reserved'
+description      'Installs/Configures nginx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.0'
+depends 'apt'
